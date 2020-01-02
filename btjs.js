3a71d32f67fb21bd6b10c6f006ca8ef8db2fcc02
otisroot <nguyenquangbao0011@gmail.com>
2020-01-03 03:24:27 +0700

    update
    update

--- btjs.js
+++ btjs.js
@@ -2,6 +2,7 @@ var btn = document.querySelector('button')
 var a = document.getElementById('a')
 var b = document.getElementById('b')
 var c = document.getElementById('c')
+var p = document.getElementsByClassName('result')[0]
 btn.addEventListener('click', function() {
     changerBackground()
     checkNumberA()
@@ -16,15 +17,18 @@ function checkNumberA() {
         a.style.border = "1px solid #ccc"
         document.getElementsByTagName('small')[0].innerText = ""
         if (isNaN(aValue)) {
+            a.style.border = "1px solid red"
             document.getElementsByTagName('small')[0].innerText = "bạn phải nhập số "
+            document.getElementsByClassName('result')[0].innerText = ""
         } else {
             document.getElementsByTagName('small')[0].innerText = ""
             x = parseInt(aValue)
+
         }
     } else {
         document.getElementsByTagName('small')[0].innerText = "mời nhập số a"
         a.style.border = "1px solid red"
-        document.getElementsByClassName('result')[0].innerText = ""
+        p.innerText = ""
     }
 }
 
@@ -33,16 +37,21 @@ function checkNumberB() {
     if (!bValue == "") {
         b.style.border = "1px solid #ccc"
         document.getElementsByTagName('small')[1].innerText = ""
+
         if (isNaN(bValue)) {
+            b.style.border = "1px solid red"
             document.getElementsByTagName('small')[1].innerText = "bạn phải nhập số "
+            document.getElementsByClassName('result')[0].innerText = ""
+
         } else {
             document.getElementsByTagName('small')[1].innerText = ""
             y = parseInt(bValue)
+
         }
     } else {
         document.getElementsByTagName('small')[1].innerText = "mời nhập số b"
         b.style.border = "1px solid red"
-        document.getElementsByClassName('result')[0].innerText = ""
+        p.innerText = ""
     }
 }
 
@@ -52,29 +61,33 @@ function checkNumberC() {
         c.style.border = "1px solid #ccc"
         document.getElementsByTagName('small')[2].innerText = ""
         if (isNaN(cValue)) {
+            c.style.border = "1px solid red"
             document.getElementsByTagName('small')[2].innerText = "bạn phải nhập số "
+            document.getElementsByClassName('result')[0].innerText = ""
+
         } else {
             document.getElementsByTagName('small')[2].innerText = ""
             z = parseInt(cValue)
+
         }
     } else {
         document.getElementsByTagName('small')[2].innerText = "mời nhập số c"
         c.style.border = "1px solid red"
-        document.getElementsByClassName('result')[0].innerText = ""
+        p.innerText = ""
     }
 }
 
 let giaiPTBH = function(a, b, c) {
     let delta = (b * b) - (4 * a * c);
     if (delta < 0) {
-        document.getElementsByClassName('result')[0].innerText = "phương trình vô nghiệm"
+        p.innerText = "phương trình vô nghiệm"
     } else if (delta == 0) {
         let x = (-b) / (2 * a);
-        document.getElementsByClassName('result')[0].innerText = "PT co nghiem kep = " + x;
+        p.innerText = "PT co nghiem kep = " + x;
     } else {
         let x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
         let x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
-        document.getElementsByClassName('result')[0].innerText = "PT co 2 nghiem phan biet x1 = " + x1 + " va x2 = " + x2;
+        p.innerText = "PT co 2 nghiem phan biet x1 = " + x1 + " va x2 = " + x2;
     }
 }
 const changerBackground = () => {
